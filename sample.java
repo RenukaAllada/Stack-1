dfa8fb486ad716ebde8618edc8934153c66c6300
Renuka Allada <renuka@Sureedus-MacBook-Air.local>
2025-03-09 11:48:47 -0500

    Completed 1 exercise

--- sample.java
+++ sample.java
@@ -1,7 +1,25 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+class sample{
+    /**************PROBLEM-1**************/
+    //tc:0(n)
+//sc:0(n)
+    class Solution {
+        public int[] dailyTemperatures(int[] temperatures) {
+            if(temperatures==null || temperatures.length==0){
+                return new int[]{};
+            }
+            int n=temperatures.length;
+            int[] days=new int[n];
+            Stack<Integer> s=new Stack<>();
+            for(int i=0;i<n;i++){
+                while(!s.isEmpty() && temperatures[i]>temperatures[s.peek()]){
+                    int popped=s.pop();
+                    days[popped]=i-popped;
+                }
+                s.push(i);
+            }
+            return days;
+        }
+    }
 
-
-// Your code here along with comments explaining your approach
+    
+}
